8fc15e2072f9a868277d810d9c481031668b0b63
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-03 10:22:57 +0530

    Time: 0 ms (100%), Space: 43.3 MB (32.37%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer>ans=new ArrayList<>();
+        if(root==null) return ans;
+        Stack<TreeNode>st=new Stack<>();
+        st.push(root);
+        while(st.size()!=0){
+            TreeNode r=st.pop();
+            ans.add(r.val);
+            if(r.right!=null){
+                st.add(r.right);
+            }
+            if(r.left!=null){
+                st.add(r.left);
+            }
+        }
+        return ans;
+    }
+}
