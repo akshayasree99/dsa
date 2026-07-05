312f8933ef877ac8d47836410484c007790402d3
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-05 11:18:24 +0530

    Time: 3 ms (100%), Space: 44.7 MB (81.18%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -1,25 +1,22 @@
 class Solution {
     int mod = 1_000_000_007;
-    int[][] dp1; //part 1
-    int[][] dp2; //part2
+    int[][] dp1;
+    int[][] dp2;
 
     public int[] pathsWithMaxScore(List<String> board) {
         int n = board.size();
-        //I prefer to use an array to map our board
         char[][] grid = new char[n][n];
         for (int i=0;i<n;i++){
             grid[i] = board.get(i).toCharArray();
         }
 
-        //Initialize dp1, dp2 with 1: denoting not yet solved
         dp1 = new int[n][n];
         for (int[] d: dp1) Arrays.fill(d,-1);
         dp2 = new int[n][n];
         for (int[] d: dp2) Arrays.fill(d,-1);  
 
         int res1 = solve1(n-1,n-1,grid);
-        if (res1<0) res1=0; //Invalid/No sum -> Integer.MIN_VALUE
-
+        if (res1<0) res1=0; 
         int res2 = solve2(n-1,n-1,0,grid);
 
         int[] res = new int[2];
@@ -32,47 +29,39 @@ class Solution {
 
     private int solve1(int i, int j, char[][] grid){
         int n = grid.length;
-        if (i<0 || j<0) return Integer.MIN_VALUE; //out of bounds
-        if (grid[i][j]=='X') return Integer.MIN_VALUE; //obstacle 
-
-        if (i==0 && j==0) return 0; //"E" does not have a score, return 0
-
+        if (i<0 || j<0) return Integer.MIN_VALUE;
+        if (grid[i][j]=='X') return Integer.MIN_VALUE; 
+        if (i==0 && j==0) return 0; 
         if (dp1[i][j]!=-1) return dp1[i][j];
 
         int res = 0;
-        //Everything except S will have a value
         if (!(i==n-1 && j==n-1)) res+= grid[i][j]-'0';
 
         int up = solve1(i-1,j,grid);
         int left = solve1(i,j-1,grid);
         int dia = solve1(i-1,j-1,grid);
-        // cell score + best direction to pick
         int ans = res + Math.max(dia,Math.max(up,left));
         return dp1[i][j] = ans;
     }
 
     private int solve2(int i,int j,int score,char[][] grid){
         int n = grid.length;
-        if (i==0 && j==0 ) { //reach the end
-            // score is the same as sum from Start point?
+        if (i==0 && j==0 ) { 
             return (score==dp1[n-1][n-1])?1:0;
         }
-        //out of bounds
         if (i<0 || j<0 || grid[i][j]=='X') return 0;
-        //score is less than highest possible
         if (score<(dp1[n-1][n-1]-dp1[i][j])) return 0;
         if (dp2[i][j]!=-1) return dp2[i][j];
 
 
         int val = 0;
-        //add the score of current cell, unless we are at "S"
         if (!(i==n-1 && j==n-1)) val+=grid[i][j]-'0';
         int up  = solve2(i-1,j,score+val,grid);
         int left = solve2(i,j-1,score+val,grid);
         int dia = solve2(i-1,j-1,score+val,grid); 
-        //add ways from all directions, take care of mod
+        
         int tot = ((up+left+dia)-mod)%mod;
-        if (tot<0) tot+=mod; //take care of mod
+        if (tot<0) tot+=mod; 
         return dp2[i][j] = tot;
 
     }
