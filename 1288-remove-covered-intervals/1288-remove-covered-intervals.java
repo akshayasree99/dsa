7e2f3c21982d357fb0842bb257df8e6920b64dca
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-06 20:53:12 +0530

    Time: 8 ms (14.35%), Space: 46.4 MB (62.65%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        List<int[]> stack = new ArrayList<>();
+
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        stack.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int a = intervals[i][0], b = intervals[i][1];
+            int[] last = stack.get(stack.size() - 1);
+            int x = last[0], y = last[1];
+
+            if (x <= a && b <= y) continue;
+
+            stack.add(intervals[i]);
+        }
+
+        return stack.size();
+    }
+}
