873660ae3b512d3048e9e15d5971d5b8874d00cc
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-09 05:20:25 +0530

    Time: 1 ms (13.03%), Space: 43.2 MB (52.29%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -23,16 +23,17 @@ class Solution {
         while(st.size()!=0){
             TreeNode r=st.peek();
             if(r.left!=null && !s.contains(r.left)) {
-                s.add(r.left);
                 st.push(r.left);
                 continue;                
             }
             if(r.right!=null &&!s.contains(r.right)){ 
                 st.push(r.right);
-                s.add(r.right);
                 continue;
             }
-            if(st.peek()!=null) ans.add(st.pop().val);
+            if(st.peek()!=null) {
+                s.add(r);
+                ans.add(st.pop().val);
+            }
         }
         return ans;
     }
