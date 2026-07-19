a1c1c3c7ab7df47ae1ed981c702a4093827d8577
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-19 08:07:29 +0530

    Time: 4 ms (25.77%), Space: 43.3 MB (8.14%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,4 +1,4 @@
-/////////-----
+/////////-------------------------------------------
 class Solution {
     public String smallestSubsequence(String s) {
         HashMap<Character,Integer> map=new HashMap<>();
