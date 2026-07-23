54005ec135d47ecea50408a4c8a21ff9ba5d7463
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-23 21:20:06 +0530

    Time: 1 ms (51.02%), Space: 125.5 MB (14.29%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,11 +1,11 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        int n = nums.length;
-        if (n <= 2) return n;
-        int mask = 0;
-        for (int num : nums) {
-            mask |= num;
+        int n=nums.length;
+        if(n<=2) return n;
+        int mask=0;
+        for(int num:nums){
+            mask|=num;
         }
-        return mask + 1;
+        return mask+1;
     }
 }
