8f15f2b3944b44b028e0c54968d93c489d2f8e7f
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-07-28 17:30:20 +0530

    Time: 2 ms (99.68%), Space: 61.3 MB (95.71%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxScore(int[] a, int k) {
-        int l=0,r=0,n=a.length;
-        for(int i=0;i<k;i++) l+=a[i];
-        int max=l,sum=l;
+        int sum=0,r=0,n=a.length;
+        for(int i=0;i<k;i++) sum+=a[i];
+        int max=sum;
         int i=k-1,j=n-1;
         while(i>=0){
             sum-=a[i--];
