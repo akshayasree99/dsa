1b4ef09024dc6e4ed11e9365ca96baac4823aa6f
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-08-01 09:40:54 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
         if (n % 2 == 0) return true;
-        
         int[] dp = nums.clone();
         for (int i = n - 2; i >= 0; --i) {
             for (int j = i + 1; j < n; ++j) {
