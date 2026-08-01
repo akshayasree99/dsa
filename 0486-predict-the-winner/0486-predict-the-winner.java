c35f16f594bf7951a934b5aabdd3643b7074f43f
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-08-01 09:42:14 +0530

    Time: 0 ms (100%), Space: 42.5 MB (89.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -5,10 +5,10 @@ class Solution {
         if(n%2==0) return true;
         int[] dp=nums.clone();
         for(int i=n-2;i>=0;--i){
-            for (int j = i + 1; j < n; ++j) {
-                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            for(int j=i+1;j<n;++j){
+                dp[j]=Math.max(nums[i]-dp[j],nums[j]-dp[j-1]);
             }
         }
-        return dp[n - 1] >= 0;
+        return dp[n-1]>=0;
     }
 }
