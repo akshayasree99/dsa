928ab7020a178afd9174be02557f1c83e558a3d9
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-08-10 14:53:13 +0530

    Time: 14 ms (91.38%), Space: 42.2 MB (96.34%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -3,14 +3,12 @@ class Solution {
         boolean[] dp = new boolean[n + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
-
                 if (!dp[i - j * j]) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-
         return dp[n];
     }
 }
