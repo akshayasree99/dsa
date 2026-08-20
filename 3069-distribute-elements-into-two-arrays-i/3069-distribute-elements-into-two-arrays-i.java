cdea5e3a0902ea45b72d2d75fd9eb187d0cb75d6
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-08-20 23:25:47 +0530

    Time: 1 ms (98.08%), Space: 46.5 MB (81.25%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -7,7 +7,6 @@ class Solution {
         int size2 = 1;
         arr1[0] = nums[0];
         arr2[0] = nums[1];
-
         for (int i = 2; i < nums.length; i++) {
             if (arr1[size1 - 1] > arr2[size2 - 1]) {
                 arr1[size1++] = nums[i];
@@ -15,9 +14,7 @@ class Solution {
                 arr2[size2++] = nums[i];
             }
         }
-
         int[] result = new int[nums.length];
-
         System.arraycopy(arr1, 0, result, 0, size1);
 
         System.arraycopy(arr2, 0, result, size1, size2);
