2344eb638c19368823fa08365e128f6217dd156e
Akshaya Sree Puligilla <164763446+akshayasree99@users.noreply.github.com>
2026-09-04 12:38:26 +0530

    Time: 1 ms (99.79%), Space: 46.6 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -4,14 +4,12 @@ class Solution {
         int[] suffix = new int[n];
  
         int mn = Integer.MAX_VALUE;
-        // Build suffix minimum
         for (int i = n - 1; i >= 0; i--) {
             mn = Math.min(mn, nums[i]);
             suffix[i] = mn;
         }
  
         int mx = 0;
-        // Find first index with score <= k
         for (int i = 0; i < n; i++) {
             mx = Math.max(mx, nums[i]);
             int score = mx - suffix[i];
